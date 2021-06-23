511f4b3e48a27807a7432b79cde9fbf7148daeec
Neo Minchul Chae <mcchae7@gmail.com>
2021-06-23 13:56:56 +0900

    remove ui
    will add ui as submodule

--- libs/ui/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/components/scroll-pagination/scroll-pagination';
-export * from './lib/components/scrollbar/scrollbar';
-export * from './lib/ui.models';
-export * from './lib/utils/utils';
